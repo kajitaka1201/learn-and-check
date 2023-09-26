de68655018eae2f2942f06b7e6b32358426d5817
takashi kajihara <142304219+kajitaka1201@users.noreply.github.com>
2023-09-26 23:26:23 +0000

    定数名変更

--- creat/script.js
+++ creat/script.js
@@ -1,24 +1,8 @@
-const el_input_file_02 = document.getElementById("input-file");
-const bt_file_02 = document.getElementById("bt-file");
-const el_output_02 = document.getElementById("output");
-//ボタンへのクリックをinput[type="file"]へ送る。
-function loadFile() {
-  el_input_file_02.click();
-}
-bt_file_02.addEventListener("click", loadFile);
-//ファイル情報を表示する。
-function outputFileInformation(e) {
-  const files = e.target.files;
-  if (files.length === 0) return;
-  const array_output = "name: " + files[0].name;
-  el_output_02.innerHTML = array_output;
-}
-el_input_file_02.addEventListener("change", outputFileInformation);
 //カードの追加
-const add_card = document.getElementById("add_card");
-const cards = document.getElementById("cards");
+const ADD_CARD = document.getElementById("add-card");
 function addCard() {
-  cards.insertAdjacentHTML(
+  const CARDS = document.getElementById("cards");
+  CARDS.insertAdjacentHTML(
     "beforeend",
     `<li class="card">
     <div class="flex items-center justify-center flex-wrap w-full my-5">
@@ -33,7 +17,7 @@ function addCard() {
       <label class="flex-none mx-2">
         <input type="checkbox" class="scale-125 check" />
       </label>
-      <button class="mx-2 delete_card" onclick="deleteCard()">
+      <button class="mx-2 delete-card" onclick="deleteCard()">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
           <path
             d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path>
@@ -43,46 +27,67 @@ function addCard() {
   </li>`
   );
 }
-add_card.addEventListener("click", addCard);
+ADD_CARD.addEventListener("click", addCard);
+
+//ファイル読み込み
+const INPUT_FILE = document.getElementById("input-file");
+const LOAD_FILE = document.getElementById("load-file");
+const OUTPUT_FILE = document.getElementById("output");
+//ボタンへのクリックをinput[type="file"]へ送る
+function loadFile() {
+  INPUT_FILE.click();
+}
+LOAD_FILE.addEventListener("click", loadFile);
+//ファイル情報を表示し、input要素に代入する（inputへの代入はまだ）
+function organizingFileInformation(e) {
+  //ファイル情報の表示
+  const FILES = e.target.files;
+  if (FILES.length === 0) return;
+  const LOADED_FILE_NAME = "name: " + FILES[0].name;
+  OUTPUT_FILE.innerHTML = LOADED_FILE_NAME;
+}
+INPUT_FILE.addEventListener("change", organizingFileInformation);
+
 //カードの削除 技術的に難しい
 function deleteCard() {
-  const delete_card = document.getElementsByClassName(".delete_card");
+  const DELETE_CARD = document.getElementsByClassName("delete-card");
   alert("現時点では使用することができません。\n消したいカードが有りましたら、問題と答えの入力欄をどちらも空白にしておいて下さい。\n空白のカードは保存しないようにします。");
 }
+
 //ファイルとして保存
 function saveAsFile() {
-  const file_name = document.getElementById("file_name").value;
-  const card = document.getElementsByClassName("card");
-  const object_data = Object.values(card).map((element) => {
-    const question = element.getElementsByClassName("question")[0];
-    const answer = element.getElementsByClassName("answer")[0];
-    const check = element.getElementsByClassName("check")[0];
-    const question_value = question.value;
-    const answer_value = answer.value;
-    const check_value = check.checked;
-    const object_value = {
-      question: `${question_value}`,
-      answer: `${answer_value}`,
-      check: `${check_value}`,
+  const FILE_NAME = document.getElementById("file-name").value;
+  const CARD = document.getElementsByClassName("card");
+  const OBJECT_DATA = Object.values(CARD).map((element) => {
+    const QUESTION = element.getElementsByClassName("question")[0];
+    const ANSWER = element.getElementsByClassName("answer")[0];
+    const CHECK = element.getElementsByClassName("check")[0];
+    const QUESTION_VALUE = QUESTION.value;
+    const ANSWER_VALUE = ANSWER.value;
+    const CHECK_VALUE = CHECK.checked;
+    const OBJECT_VALUE = {
+      question: `${QUESTION_VALUE}`,
+      answer: `${ANSWER_VALUE}`,
+      check: `${CHECK_VALUE}`,
     };
-    return object_value;
+    return OBJECT_VALUE;
   });
-  const nonblankCard = Object.values(object_data).filter((element) => {
-    const question_value = element.question;
-    const answer_value = element.answer;
-    const boolean = !(question_value == false || answer_value == false);
-    return boolean;
+  const NON_BLANK_CARD = Object.values(OBJECT_DATA).filter((element) => {
+    const QUESTION_VALUE = element.question;
+    const ANSWER_VALUE = element.answer;
+    const BOOLEAN_VALUE = !(QUESTION_VALUE == false || ANSWER_VALUE == false);
+    return BOOLEAN_VALUE;
   });
-  const json_data = {
-    name: file_name,
-    contents: nonblankCard,
+  const JSON_DATA = {
+    name: FILE_NAME,
+    contents: NON_BLANK_CARD,
   };
-  const file_data = JSON.stringify(json_data);
-  const blob = new Blob([file_data], {type: "application/json"});
-  const link = document.createElement("a");
-  document.body.appendChild(link);
-  link.href = window.URL.createObjectURL(blob);
-  link.download = `${file_name}.learn-and-check.json`;
-  link.click();
-  document.body.removeChild(link);
+  const FILE_DATA = JSON.stringify(JSON_DATA);
+  const BLOB = new Blob([FILE_DATA], {type: "application/json"});
+  const LINK = document.createElement("a");
+  document.body.appendChild(LINK);
+  LINK.href = window.URL.createObjectURL(BLOB);
+  LINK.download = `${FILE_NAME}.learn-and-check.json`;
+  LINK.click();
+  document.body.removeChild(LINK);
 }
