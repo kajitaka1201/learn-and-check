388c231a1b7bf5f1b79a4058992fe4d52cd6155d
takashi kajihara <142304219+kajitaka1201@users.noreply.github.com>
2024-03-29 09:37:53 +0000

    FileDataのuseState作った（カードの追加、削除機能を付けた、読み込み、保存、開始はまだ）

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,57 +1,88 @@
+"use client";
 //import
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import Card from "@/components/ui-elements/card";
+import { useState } from "react";
+import { v4 as createUUID } from "uuid";
+
+//type
+export type FileType = {
+  name?: string;
+  contents: {
+    question: string;
+    answer: string;
+    isCheck: boolean;
+    hint?: string;
+    explanation?: string;
+    id: string;
+  }[];
+};
 
 //createページ内容
 export default function Create() {
+  //fileData
+  const [fileData, setFileData] = useState<FileType>();
+  console.log(fileData);
+  //function
+  function addCard() {
+    setFileData({
+      ...fileData,
+      contents: [
+        ...(fileData?.contents || []),
+        { question: "", answer: "", isCheck: false, id: createUUID() },
+      ],
+    });
+  }
+
   return (
     <>
       <main className="my-10 mx-5">
         <div className="flex flex-wrap flex-col items-center text-center gap-10 max-w-7xl mx-auto">
-          <div className="w-full border-blue-600 border-2 rounded-2xl px-20 py-5">
-            <h1 className="text-4xl">単語帳設定</h1>
-            <div className="flex flex-col items-stretch">
-              <div className="my-4">
-                <label className="flex justify-between items-center hover:bg-gray-200">
-                  チェックボックス機能を使用する
-                  <Checkbox className="w-6 h-6" />
-                </label>
-              </div>
-              <div className="my-4">
-                <label className="flex justify-between items-center hover:bg-gray-200">
-                  解答欄機能を使用する
-                  <Checkbox className="w-6 h-6" />
-                </label>
-              </div>
-            </div>
-          </div>
           <div className="w-full border-blue-600 border-2 rounded-2xl grid gap-5 px-20 py-5">
             <div>
               <h1 className="text-4xl">入力欄</h1>
               <p>以下の入力欄に入力するか、規定のファイル形式のファイルを読み込んで下さい。</p>
               <Input type="file" accept=".learn-and-check.json" className="hidden" />
               <div className="flex justify-center items-center">
-                <Button className="p-2 rounded-lg hover:bg-blue-400 w-40">ファイルを読み込む</Button>
+                <Button className="p-2 rounded-lg hover:bg-blue-400 w-40">
+                  ファイルを読み込む
+                </Button>
                 <div className="[max-width:50%]" />
               </div>
             </div>
             <div className="grid gap-5">
               <label className=" w-full flex justify-center">
-                <Input type="text" maxLength={100} placeholder="タイトルを入力して下さい" className="border-[#767676] border border-solid rounded-[5px] flex-1" />
+                <Input
+                  type="text"
+                  defaultValue={fileData?.name}
+                  onChange={(e) => setFileData({ ...fileData, name: e.target.value } as FileType)}
+                  placeholder="タイトルを入力して下さい"
+                  className="border-[#767676] border border-solid rounded-[5px] flex-1"
+                />
               </label>
               <div className="grid gap-3 w-full list-decimal min-w-[12.5rem]">
-                <Card index={1} />
-                <Card index={2} />
+                {fileData?.contents?.map((content, index) => (
+                  <Card
+                    key={content.id}
+                    index={index}
+                    content={content}
+                    setFileData={setFileData}
+                  />
+                ))}
               </div>
               <div>
-                <Button className="p-2 rounded-lg hover:bg-blue-400 w-40">カードを追加</Button>
+                <Button className="p-2 rounded-lg hover:bg-blue-400 w-40" onClick={addCard}>
+                  カードを追加
+                </Button>
               </div>
             </div>
           </div>
           <div className="flex">
-            <Button className="p-2 rounded-lg hover:bg-blue-400 mx-2 w-40">ファイルとして保存</Button>
+            <Button className="p-2 rounded-lg hover:bg-blue-400 mx-2 w-40">
+              ファイルとして保存
+            </Button>
             <Button className="p-2 rounded-lg hover:bg-blue-400 mx-2 w-40">単語帳開始</Button>
           </div>
         </div>
@@ -64,7 +95,11 @@ export default function Create() {
                     <p className="text-5xl">問題文</p>
                   </div>
                   <div className="w-full text-center flex justify-center my-2">
-                    <Input type="text" placeholder="回答欄" className="border-[#767676] border border-solid rounded-[10px] flex-1 text-2xl p-2" />
+                    <Input
+                      type="text"
+                      placeholder="回答欄"
+                      className="border-[#767676] border border-solid rounded-[10px] flex-1 text-2xl p-2"
+                    />
                     <Button className="ml-4 p-2 rounded-lg hover:bg-blue-400 w-20">確認</Button>
                   </div>
                   <div className="w-full text-center my-2">
@@ -77,7 +112,12 @@ export default function Create() {
               <div className="flex justify-center items-center">
                 <div>
                   <Button className="hover:bg-blue-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      fill="#000000"
+                      viewBox="0 0 256 256">
                       <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H107.31l18.35,18.34a8,8,0,0,1-11.32,11.32l-32-32a8,8,0,0,1,0-11.32l32-32a8,8,0,0,1,11.32,11.32L107.31,120H168A8,8,0,0,1,176,128Z" />
                     </svg>
                   </Button>
@@ -90,7 +130,12 @@ export default function Create() {
                 </div>
                 <div>
                   <Button className="hover:bg-blue-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      fill="#000000"
+                      viewBox="0 0 256 256">
                       <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm45.66-93.66a8,8,0,0,1,0,11.32l-32,32a8,8,0,0,1-11.32-11.32L148.69,136H88a8,8,0,0,1,0-16h60.69l-18.35-18.34a8,8,0,0,1,11.32-11.32Z" />
                     </svg>
                   </Button>
--- components/ui-elements/card.tsx
+++ components/ui-elements/card.tsx
@@ -2,22 +2,55 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { FileType } from "@/app/create/page";
 
-export default function Card({index}: {index: number}) {
+export default function Card({
+  index,
+  content,
+  setFileData,
+}: {
+  index: number;
+  content: FileType["contents"][number];
+  setFileData: React.Dispatch<React.SetStateAction<FileType | undefined>>;
+}) {
   return (
     <div className="flex items-center justify-center flex-wrap w-full gap-2">
-      <p>{index}</p>
+      <p>{index + 1}</p>
       <div className="flex flex-1 gap-2">
         <label className="flex-1">
-          <Input type="text" placeholder="問題を入力して下さい" className="w-full border-[#767676] border border-solid rounded-[5px] min-w-[10rem]" />
+          <Input
+            type="text"
+            defaultValue={content.question}
+            placeholder="問題を入力して下さい"
+            className="w-full border-[#767676] border border-solid rounded-[5px] min-w-[10rem]"
+          />
         </label>
         <label className="flex-1">
-          <Input type="text" placeholder="解答を入力して下さい" className="w-full border-[#767676] border border-solid rounded-[5px] min-w-[10rem]" />
+          <Input
+            type="text"
+            defaultValue={content.answer}
+            placeholder="解答を入力して下さい"
+            className="w-full border-[#767676] border border-solid rounded-[5px] min-w-[10rem]"
+          />
         </label>
       </div>
-      <Checkbox className="w-6 h-6" />
-      <Button className=" hover:bg-blue-400">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256">
+      <Checkbox className="w-6 h-6" defaultChecked={content.isCheck} />
+      <Button
+        className=" hover:bg-blue-400"
+        onClick={() =>
+          setFileData((prev) => {
+            return {
+              ...prev,
+              contents: prev?.contents?.filter((c) => c.id !== content.id) || [],
+            };
+          })
+        }>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="#000000"
+          viewBox="0 0 256 256">
           <path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z" />
         </svg>
       </Button>
