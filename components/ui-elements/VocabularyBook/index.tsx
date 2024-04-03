fe2ffcae8965008dcd758a149bf8724f9bd8a007
takashi kajihara <142304219+kajitaka1201@users.noreply.github.com>
2024-04-03 23:04:02 +0000

    escapeKey、outsideでの閉じるのを拒否

--- components/ui-elements/VocabularyBook/index.tsx
+++ components/ui-elements/VocabularyBook/index.tsx
@@ -35,11 +35,14 @@ export default function VocabularyBook() {
       <DialogTrigger className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow h-9 p-2 rounded-lg hover:bg-blue-400 mx-2 w-40">
         単語帳開始
       </DialogTrigger>
-      <DialogContent className="max-w-none w-4/5 h-4/5 flex flex-col">
+      <DialogContent
+        className="max-w-none w-4/5 h-4/5 flex flex-col"
+        onEscapeKeyDown={e => e.preventDefault()}
+        onPointerDownOutside={e => e.preventDefault()}>
         <DialogHeader className="flex-none">
           <DialogTitle>単語帳</DialogTitle>
         </DialogHeader>
-        {mode === "set" && <Settings setSettings={setSettings} start={()=>setMode("use")}/>}
+        {mode === "set" && <Settings setSettings={setSettings} start={() => setMode("use")} />}
         {mode === "use" && <Use />}
       </DialogContent>
     </Dialog>
