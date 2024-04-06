b078e585347c4078987d845e9356f3addc15ed84
takashi kajihara <142304219+kajitaka1201@users.noreply.github.com>
2024-04-06 07:03:40 +0000

    v2..0.1決定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+const SUB_DIRECTORY = "/learn-and-check";
+const isProd = process.env.NODE_ENV == "production";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? SUB_DIRECTORY : "",
+  assetPrefix: isProd ? SUB_DIRECTORY : "",
+  publicRuntimeConfig: {
+    basePath: isProd ? SUB_DIRECTORY : "",
+  },
+};
 
 export default nextConfig;
