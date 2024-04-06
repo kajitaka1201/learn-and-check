a9b610f4097efe56e4ce939035f612f3db416dec
takashi kajihara <142304219+kajitaka1201@users.noreply.github.com>
2024-04-06 08:12:05 +0000

    画像パス修正

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,12 @@
 export default function NotFound() {
   return (
-    <>
-      <main className="flex-1">
-        <div className="grid w-full max-w-7xl justify-items-center gap-12 p-12 text-center">
-          <div>
-            <h1 className="text-4xl">404 Not Found</h1>
-            <p>お探しのページが見つかりませんでした。</p>
-          </div>
+    <main>
+      <div className="mx-auto grid w-full max-w-7xl justify-items-center gap-12 p-12 text-center">
+        <div>
+          <h1 className="text-4xl">404 Not Found</h1>
+          <p>お探しのページが見つかりませんでした。</p>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
